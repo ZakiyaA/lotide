42e89f641abd479203d2976abe0f4ae3bf4a55b3
ZakiyaAlfughi <zakiyaalfughi@gmail.com>
2021-11-25 01:04:41 +0000

    Implement counOnly

---
+++ countOnly.js
@@ -0,0 +1,48 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+const countOnly = function(allItems, itemsToCount) {
+  const result = {};
+  const newResult = {};
+  for (let item in itemsToCount) {
+    if (itemsToCount[item]) {
+      result[item] = 0;
+    }
+  }
+  for (let item of allItems) {
+    if (itemsToCount[item]) {
+      result[item] += 1;
+    }
+  } 
+  for (let item in result) {
+    if (result[item] !== 0) {
+      newResult[item] = result[item];
+    }
+  }
+  return newResult;
+};
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, {"Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
