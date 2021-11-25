1ceb67894fab851546d3e29ff3734307ac780899
ZakiyaAlfughi <zakiyaalfughi@gmail.com>
2021-11-25 05:41:16 +0000

    Implement letterPositions

---
+++ letterPositions.js
@@ -0,0 +1,21 @@
+const letterPositions = function(sentence) {
+  const results = {};
+  let counter = -1;
+  //logic to update results here
+  for (const letter of sentence) {
+    if (letter !== " ") {
+      if (results[letter]) {
+        counter += 1;
+        results[letter].push(counter);
+      } else {
+        counter += 1;
+        results[letter] = [counter];
+      }
+    } else {
+      counter += 1;
+    }
+  }
+  return results;
+};
+
+console.log(letterPositions("lighthouse in the house"));
