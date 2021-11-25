7b0d900f630c032b4b67e10840138ea75c06e6b0
ZakiyaAlfughi <zakiyaalfughi@gmail.com>
2021-11-25 18:32:06 +0000

    Implement countLetters

---
+++ countLetters.js
@@ -0,0 +1,21 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+const countLetters = function(stringArray) {
+  const results = {};
+  for (let letter of stringArray) {
+    if (results[letter]) {
+      results[letter] += 1;
+    } else {
+      results[letter] = 1;
+    }
+  }
+  return results;
+};
+
+console.log(countLetters("lighthouse in the house"));
