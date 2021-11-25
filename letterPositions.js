1d3f311f5f2f451406bc8941eb4feea467639693
ZakiyaAlfughi <zakiyaalfughi@gmail.com>
2021-11-25 05:47:08 +0000

    Implement letterPositions

--- letterPositions.js
+++ letterPositions.js
@@ -1,3 +1,23 @@
+const eqArrays = function(array1, array2) {
+  let output = true;
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let element = 0; element < array1.length; element ++) {
+    if (array1[element] !== array2[element]) {
+      return false;
+    }
+  }
+  return output;
+};
+// FUNCTION IMPLEMENTATION
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
 const letterPositions = function(sentence) {
   const results = {};
   let counter = -1;
@@ -18,4 +38,4 @@ const letterPositions = function(sentence) {
   return results;
 };
 
-console.log(letterPositions("lighthouse in the house"));
+assertArraysEqual(letterPositions("hello"), [1]);
