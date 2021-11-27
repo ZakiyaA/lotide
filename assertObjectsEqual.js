9606a5de997da01cc5abfab8848b91d90d3cdc81
ZakiyaAlfughi <zakiyaalfughi@gmail.com>
2021-11-27 07:46:44 +0000

    Implement assertObjectsEqual

---
+++ assertObjectsEqual.js
@@ -0,0 +1,51 @@
+// FUNCTION IMPLEMENTATION
+const assertObjectsEqual = function(object1, object2) {
+  const inspect = require('util').inspect;
+  if (eqObjects(object1, object2)) {
+    console.log(`✅✅✅ Assertion Passed: ${inspect(object1)} === ${inspect(object2)}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${inspect(object1)} !== ${inspect(object2)}`);
+  }
+};
+// EqArray function implementation
+const eqArrays = function(array1, array2) {
+  let output = true;
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let element = 0; element < array1.length; element++) {
+    if (array1[element] !== array2[element]) {
+      return false;
+    }
+  }
+  return output;
+};
+// EqArray function implementation
+const eqObjects = function(object1, object2) {
+  const object1Keys = Object.keys(object1);
+  const object2Keys = Object.keys(object2);
+  if (object1Keys.length !== object2Keys.length) {
+    return false;
+  }
+  for (let key of object1Keys) {
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+      if (!eqArrays(object1[key], object2[key])) {
+        return false;
+      }
+    } else {
+      if (object1.key !== object2.key) {
+        console.log(key, object1[key], object2[key]);
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+eqObjects(cd, dc); // => true
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+eqObjects(cd, cd2); // => false
+assertObjectsEqual(cd,dc);
