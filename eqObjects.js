0c2616a971ab66abc03f361e0b9736933021fc49
ZakiyaAlfughi <zakiyaalfughi@gmail.com>
2021-11-27 06:49:38 +0000

    Implement eqObject

---
+++ eqObjects.js
@@ -0,0 +1,49 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+// EqArray function implementation
+const eqArrays = function(array1, array2) {
+  let output = true;
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let element = 0; element < array1.length; element++) {
+    if (array1[element] !== array2[element]) {
+      return false;
+    }
+  }
+  return output;
+};
+//EqObject function implementation 
+const eqObjects = function(object1, object2) {
+  const object1Keys = Object.keys(object1);
+  const object2Keys = Object.keys(object2);
+  if (object1Keys.length !== object2Keys.length) {
+    return false;
+  }
+  for (let key of object1Keys) {
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+      if (!eqArrays(object1[key], object2[key])) {
+        return false;
+      }
+    } else {
+      if (object1.key !== object2.key) {
+        console.log(key, object1[key], object2[key]);
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+console.log(eqObjects(cd, dc)); // => true
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+console.log(eqObjects(cd, cd2)); // => false
