a39538dfe873d11093f312e173718e0bae332af3
ZakiyaAlfughi <zakiyaalfughi@gmail.com>
2021-11-29 22:50:53 +0000

    Implement tail

--- tail.js
+++ tail.js
@@ -1,3 +1,5 @@
+
+const assertEqual = require('./assertEqual');
 // Function returns the first item in the array
 const tail = function(array) {
   if (array.length > 2) {
@@ -7,31 +9,4 @@ const tail = function(array) {
   }
 };
 
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-};
-
-// Find if the Two arrays are equal or not
-const eqArrays = function(array1, array2) {
-  let output = true;
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let element = 0; element < array1.length; element ++) {
-    if (array1[element] !== array2[element]) {
-      return false;
-    }
-  }
-  return output;
-};
-
-
-
-// TEST CODE
-tail(["Hello","Lighthouse","Labs"]);
-assertEqual(tail(["Hello","Lighthouse","Labs"]), ["Lighthouse","Labs"]); 
+module.exports = tail;
